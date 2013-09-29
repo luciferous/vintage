fa58a0b1a00b5807074cfbcaaa0106eca2a7bcfd
Neuman Vong <neuman.vong@gmail.com>
2013-09-29 09:53:12 -0700

    pingtest client and server

--- tests/Ping.hs
+++ tests/Ping.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Monad.IO.Class (liftIO)
+import System.Environment (getArgs)
 
 import Ping.GenTypes
 import Vintage
@@ -10,6 +11,12 @@ app = do
     msg <- send Ping
     liftIO $ putStrLn (show (msg :: Pong))
 
+server :: Ping -> IO Pong
+server Ping = return Pong
+
 main :: IO ()
-main = runClient "localhost" 9090 app
+main = getArgs >>= \args -> case args of
+    ["client"] -> runClient "localhost" 9090 app
+    ["server"] -> runServer 9090 server
+    _          -> putStrLn "Usage: pingtest [client|server]"
 
